b017fbcffcbd31f7077c2c4e97d049ff164fc4c4
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-12 13:12:36 +0530

    Create DoWhl.java

---
+++ DoWhl.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+Class Dowhl{
+       public static void main(String[] args) {
+              int i=1;
+              do {
+                      System.out.println("Sujal");
+                      i++;
+              }While(i<=5);
+       }
+}
