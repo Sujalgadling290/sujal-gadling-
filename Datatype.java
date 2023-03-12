c72e84c8d94b713bcc6524bac388abd9d7ed9a4b
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-12 12:22:45 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,12 @@
+class Datatype{
+      public static void main(Strimg[] args) {
+             byte b=32;
+             short s=1508;
+             int i=96595985;
+             long l=369252825;
+             System.out.println(b);
+             System.out.println(S);
+             System.out.println(i);
+             System.out.println(l);
+      }
+}
