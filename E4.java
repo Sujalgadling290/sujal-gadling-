dcc041384e60f1e059008860115879613a44ed2d
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-12 13:33:05 +0530

    Create E4.java

---
+++ E4.java
@@ -0,0 +1,9 @@
+class E4{
+        Static int i=5;
+        Static public void show(){
+               System.out.println("value of i="+1);
+        }
+        public static void main(String[] args) {
+               E4.show();
+        }
+}
