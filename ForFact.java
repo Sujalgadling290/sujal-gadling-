999ac082775145b6bf9050b0591951f3657ae100
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-14 15:42:15 +0530

    Create ForFact.java

---
+++ ForFact.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+class ForFact{
+        public static void main(String[] args) {
+                Scanner sc=new Scanner(Systenm.in);
+                System.out.print("Enter any number: ");
+                int no=sc.nextInt();
+                int fact=1;
+                for (int i=0;i<no;--no) {
+                         fact=fact*no;
+                }
+          Syastem.out.print("The Factorial is "+fact);
+        }
+}
+                     
+                                
+        
