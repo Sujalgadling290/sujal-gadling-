8fa95fc48bfc40812e70b9608bd21ab87d3942da
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-16 22:19:19 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,10 @@
+class pattern5{
+        public static void main(String[] args) {
+               for (int i=1;i<4 ;++i) {
+                        for (int j=4;j>=i ;j++) {
+                                 System.out.println();
+                        }
+                 System.out.pintln();
+               }
+        }
+}
