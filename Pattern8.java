3949dc15251135fa38493ea9afc02828cfff9d86
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-16 22:37:27 +0530

    Create Pattern8.java

---
+++ Pattern8.java
@@ -0,0 +1,13 @@
+class Pattern8{
+        public static void main(String[] args) {
+               for (int i=1;i<=4 ;++i ) {
+                        for (int k=1;k<=i ;++k ) {
+                                 System.out.print("*");
+                        }
+                        for (int j=4;j>=i ;--j ) {
+                                 System.out.print("*");
+                        }
+                 System.out.println();
+               }
+        }
+}
