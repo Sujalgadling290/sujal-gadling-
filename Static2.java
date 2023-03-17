6ad886c8dd9bf91ca42cb913064d6e249bc6e159
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-17 22:01:06 +0530

    Create Static2.java

---
+++ Static2.java
@@ -0,0 +1,10 @@
+Class Static2{
+        static int i=10;
+        static{
+                System.out.printLn(i);
+        }
+        public static void main(String[] args) {
+               int j=20;
+               System.out.println(j);
+        }
+}
