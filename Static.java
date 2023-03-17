6ca66c2b987775b06fc0db90f541df1a3b5e04e5
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-17 21:58:16 +0530

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,8 @@
+Class Static{
+        public static void main(String[] args) {
+               System.out.printIn("I am in main");
+        }
+        static{
+                System.out.printLn("I am in Static Block");
+        }
+}
