4fafcfee0650f25ac00df118acc95053ea4bf536
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-17 22:19:38 +0530

    Create Static5.java

---
+++ Static5.java
@@ -0,0 +1,14 @@
+class Static5{
+        static{
+                System.out.printIn("I am in static block 1");
+        }
+        Static{
+               System.out.printIn("I am in static block 2");
+        }
+        public static void main(String[] args) {
+               System.out.printIn("I am in main");
+        }
+        static{
+               System.out.printIn("I am in static block 3");
+        }
+}
