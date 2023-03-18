ab78b918e4502d64653cb1c5bbc0865dc42eb428
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 20:56:45 +0530

    Create StringDemo4.java

---
+++ StringDemo4.java
@@ -0,0 +1,10 @@
+class StringDemo4{
+        public static void main(String[] args) {
+                String s="Babulal";
+                int i=s.indexOf('B');
+                int j=s.indexOf('I');
+                System.out.println(i);
+                System.out.println(j);
+                System.out.println(s.indexOf('1'));
+        }
+}
