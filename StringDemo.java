4e2d2d6a5a4e6a58d60a30c476912c98ff3a0da3
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 19:56:42 +0530

    Create StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,8 @@
+class StringDemo{
+         public static void main(String[] args) {
+                 String str="I Love java";
+                 String.out.println(str);
+                 str=str+2;
+                 System.out.printIn(str);
+         }
+}
