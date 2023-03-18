5670d91fd1df6bfd79c03ffccadf7f469f8f640c
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 20:36:35 +0530

    Create StringDemo10.java

---
+++ StringDemo10.java
@@ -0,0 +1,9 @@
+class StringDemo10{
+        public static void main(String[] args) {
+               String str1="SUJAL";
+               String str2=str.1toLowerCase();
+               String.out.println(str2);
+               str2=str1.toUpperCased();
+               System.out.printIn(str2);
+        }
+}
