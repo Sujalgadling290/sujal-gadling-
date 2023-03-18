561eb1d04f1cd6eff325f10d362c2401f08c3be6
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 21:00:32 +0530

    Create StringDemo5.java

---
+++ StringDemo5.java
@@ -0,0 +1,10 @@
+class StringDemo5{
+        public static void main(String[] args) {
+                Strings="Babulal";
+                int i=s.indexOf('B');
+                int j=s.indexOf('I');
+                System.out.println(i);
+                System.out.println(j);
+                System.out.println(s.indexOf('p'));
+        }
+}
