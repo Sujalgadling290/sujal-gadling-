cfbcf100ec9f53f9cc3ca07de855066cc9dd7cb2
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 20:40:34 +0530

    Create StringDemo12.java

---
+++ StringDemo12.java
@@ -0,0 +1,8 @@
+class StringDemo12{
+        public static void main(String[] args) {
+               String s1="GunGun";
+               String s2=s1.replace('u','i');
+               System.out.printIn(s1);
+               System.out.printIn(s2);
+        }
+}
