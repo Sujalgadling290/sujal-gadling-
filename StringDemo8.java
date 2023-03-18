431824c85f9e6d07b2f806ac30b593b33c8798b9
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 21:27:45 +0530

    Create StringDemo8.java

---
+++ StringDemo8.java
@@ -0,0 +1,11 @@
+class StringDemo8{
+         public static void main(String[] args) {
+                 String s1=""Sujal";
+                 boolean b=s1.equals("Sujal");
+                 System.out.println(b);
+                 Sytem.out.println(s1.equals("Sujal"));
+                 String s2="Prashant";
+                 boolean b1=s1.equals(s2);
+                 Sytem.out.println(b1);
+         }
+}
