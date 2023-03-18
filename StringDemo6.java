84ea8b0d240c16e81fead8538ea63fe8ffadccd8
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 21:09:30 +0530

    Create StringDemo6.java

---
+++ StringDemo6.java
@@ -0,0 +1,10 @@
+class StringDemo6{
+         public static void main(String[] args) {
+                 String s="Babulal";
+                 System.out.println(s.startswith("B"));
+                 System.out.println(s.startsWith("Babu"));
+                 System.out.println(s.startsWith("Babulal"));
+                 System.out.println(s.startsWith("babulal"));
+                 System.out.println(s.startsWith("lal"));
+         }
+}
