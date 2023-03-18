b0bb500db4ab95ec7f354928578447c8707eb5b5
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 21:33:22 +0530

    Create StringDemo9.java

---
+++ StringDemo9.java
@@ -0,0 +1,11 @@
+class StringDemo9{
+        public static void main(string[] args) {
+               String s1="Sujal";
+               boolean b=s1.equalsIgnoreCase("sujal);
+               System.out.println(b);
+               System.out.println(s1.equalsIgnoreCase("Sujal"));
+               String s2="Prashant";
+               boolean b1=s1.equalsIgnoreCase(s2);
+               System.out.println(b1);
+          }
+      }                                             
