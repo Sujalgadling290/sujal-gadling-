77b54e57b5e6e5ac43110902ec6eb637e1402272
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 21:49:56 +0530

    Create StudentClass.java

---
+++ StudentClass.java
@@ -0,0 +1,11 @@
+class Students{
+        public void show(String name,int rollno){
+               System.out.print("The Name is "+name+" and Roll No is "+rollno);
+        }
+}
+class StudentsClass{
+        public static void main(String[] args) {
+               Student obj=new Student();
+               obj.show("Sujal",290);
+        }
+}
