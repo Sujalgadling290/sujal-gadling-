7c77a67c8d52d8b21aa0418d245d2b3a68b6d246
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-18 19:53:52 +0530

    Create StaticKeyword2.java

---
+++ StaticKeyword2.java
@@ -0,0 +1,7 @@
+class StaticKeyword2.{
+        static int n;
+        public static void main(String[] args) {
+                n=10;
+                System.out.println("Value of n: "+n);
+        }
+}
