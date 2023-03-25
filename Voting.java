38fe793831829dc3a333f789eef081d46f873c62
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-25 22:58:25 +0530

    Create Voting.java

---
+++ Voting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Voting{
+        public static void main(String[] args) {
+               SCanner sc=new Scanner(System.in);
+               System.out.print("Enter Your Age: ");
+               int age=sc.nextInt();
+                if (age>=18) {
+                        System.out.print("You are Eligible for Voting.");
+                }else {
+                       System.out.print("You are Not Eligible for Voting.");
+                }
+        }
+}
