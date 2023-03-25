64cf03998527d49f250c7f9fa7f894c0b2f7de29
Sujalgadling290 <127419983+Sujalgadling290@users.noreply.github.com>
2023-03-25 22:42:43 +0530

    Create SumSer.java

---
+++ SumSer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+class SumSer{
+        public static void main(String[] args) {
+               Scanner sc=new Scanner(System.in);
+               System.out.print("Enter the value of x: ");
+               int x=sc.nextInt();
+               double sum-1;
+               for (int i=1;i<-x ;i++) {
+                      sum=sum+(Math.pow(x,1)/1);
+               }
+               System.out.print("The Sum Of Series is "+Sum);
+        }
+}
